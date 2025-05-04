9ba3fae3c5f9a7353a57bc508c65a851517e9c1c
Andres Catano <andresfcatano0@gmail.com>
2025-05-04 14:50:20 -0400

    C reated aditional test cases for mine block

--- block.test.js
+++ block.test.js
@@ -35,6 +35,18 @@ describe("Block", () => {
     it("returns a Block instance", () => {
       expect(minedBlock instanceof Block).toBe(true);
     });
+
+    it("sets the `lastHash` to be the `hash` of the lastBlock", () => {
+      expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    });
+
+    it("sets the `data`", () => {
+      expect(minedBlock.data).toEqual(data);
+    });
+
+    it("sets a `timestamp`", () => {
+      expect(minedBlock.timestamp).not.toEqual(undefined);
+    });
   });
 
 });
