e2ea499a194ec5e82a8366bf4003232ce11bfa1e
Andres Catano <andresfcatano0@gmail.com>
2025-05-15 17:39:00 -0400

    Added comments to cryptoHash js file

--- crypto-hash.js
+++ crypto-hash.js
@@ -4,10 +4,13 @@ const crypto = require("crypto");
 // ... spread operator can handle any number of incoming inputs
 // combined inputs into a single array
 const cryptoHash = (...inputs) => {
+  // Create a SHA-256 hash object
   const hash = crypto.createHash("sha256");
 
-  hash.update(inputs.sort().join(" "));
+  // Sort inputs, join them as a string, and feed them into the hash function
+  hash.update(inputs.sort().join(" ")); 
 
+  // Return the resulting hash in hexadecimal format
   return hash.digest("hex");
 };
 
