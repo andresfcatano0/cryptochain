89f9f9f29c950c0e338fcbaa9688a33f250c6422
Andres Catano <andresfcatano0@gmail.com>
2025-05-17 13:58:20 -0400

    Created initial describe test cases for isValidChain function

--- blockchain.test.js
+++ blockchain.test.js
@@ -19,4 +19,32 @@ describe("Blockchain", () => {
     expect(blockchain.chain[blockchain.chain.length - 1].data)
       .toEqual(newData);
   });
-})
+
+  describe("isValidChain()", () => {
+    describe("when the chain does not start with the genesis block", () => {
+      it("returns false", () => {
+
+      });
+    });
+
+    describe("when the chain starts with the genesis block and has multiple blocks", () => {
+      describe("and a lastHash reference has changed", () => {
+        it("returns false", () => {
+
+        });
+      });
+
+      describe("and the chain contains a block with an invalid field", () => {
+        it("returns false", () => {
+
+        });
+      });
+
+      describe("and the chain does not contain any invalid blocks", () => {
+        it("returns true", () => {
+
+        });
+      });
+    });
+  });
+});
